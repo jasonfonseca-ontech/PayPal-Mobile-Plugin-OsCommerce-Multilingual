d37ba9a71b3f4904e2208358385906c4ec735f9a
jlivas <jlivas@ontech.com.au>
2012-09-26 12:23:30 +1000

    Update mobile/cart.php

--- mobile/cart.php
+++ mobile/cart.php
@@ -105,11 +105,10 @@
 </td>
 </tr>
 </table>
-
 <div style="text-align:center; padding-top:10px;">
-	<a rel="external" href="<?php echo IPN_HANDLER ?>?type=ec">
-		    <img id="paypalbutton" src="<?php echo $_SESSION['PaypalLanguages']['checkoutWithPaypal'] ?>" />
-		    <img style="display:none;" src="<?php echo $_SESSION['PaypalLanguages']['checkoutWithPaypal'] ?>" />
+	<a rel="external" href="./ext/modules/payment/paypal/express.php">
+		    <img id="paypalbutton" src="./mobile/images/btn_checkout_278x43.png" />
+		    <img style="display:none;" src="./mobile/images/btn_checkout_278x43down.png" />
     </a>
 </div>
 
