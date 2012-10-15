69c55d0a3cbc0e31ca9e798059def0a2e67f5a51
jfonseca <jfonseca@ontech.com.au>
2012-10-16 09:56:03 +1100

    1054 - Unknown column 's.specials_new_products_price' in 'order clause' fix

--- mobile.php
+++ mobile.php
@@ -190,11 +190,16 @@ function matchhome() {
 }
 if(matchhome()) {
   	$select_column_list = 'pd.products_name, p.products_image, ';
-    $listing_sql = "select " . $select_column_list . " p.products_id, p.manufacturers_id, p.products_price, p.products_tax_class_id, IF(s.status, s.specials_new_products_price, NULL) as specials_new_products_price, IF(s.status, s.specials_new_products_price, p.products_price) as final_price from " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_PRODUCTS . " p left join " . TABLE_MANUFACTURERS . " m on p.manufacturers_id = m.manufacturers_id left join " . TABLE_SPECIALS . " s on p.products_id = s.products_id, " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c where p.products_status = '1' and p.products_id = p2c.products_id and pd.products_id = p2c.products_id and pd.language_id = '" . (int)$languages_id . "'";
+    $listing_sql = "select " . $select_column_list . " p.products_id, p.manufacturers_id, p.products_price, 
+	p.products_tax_class_id, IF(s.status, s.specials_new_products_price, NULL) as specials_new_products_price, 
+	IF(s.status, s.specials_new_products_price, p.products_price) as final_price from " . TABLE_PRODUCTS_DESCRIPTION . " pd, " 
+	. TABLE_PRODUCTS . " p left join " . TABLE_MANUFACTURERS . " m on p.manufacturers_id = m.manufacturers_id left join " 
+	. TABLE_SPECIALS . " s on p.products_id = s.products_id, " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c 
+	where p.products_status = '1' and p.products_id = p2c.products_id and pd.products_id = p2c.products_id and pd.language_id = '" . (int)$languages_id . "'
+	order by s.specials_new_products_price desc, p.products_date_added desc";
     $categories_query = tep_db_query("select count(*) as total from " . TABLE_CATEGORIES 
 	. " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd where c.parent_id = '" 
-	. (int)$category_links[$i] . "' and c.categories_id = cd.categories_id and cd.language_id = '" . (int)$languages_id . "'  
-	order by s.specials_new_products_price desc, p.products_date_added desc");
+	. (int)$category_links[$i] . "' and c.categories_id = cd.categories_id and cd.language_id = '" . (int)$languages_id . "'");
     $categories = tep_db_fetch_array($categories_query);      
         
 	global $listing_sql, $db, $listing_query, $categories;
