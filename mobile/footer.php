647542664781e56576598ce8d57cf526273b6230
jasonfonseca-ontech <jason@ezimerchant.com>
2012-10-18 16:13:34 +1200

    Return to Desktop

--- mobile/footer.php
+++ mobile/footer.php
@@ -1,13 +1,12 @@
-
-</div><!-- /content -->
-
-	</div><!-- /page -->
-
+	<div id="page_footer" style="text-align:center;padding-top:10px;">
+	     <a href="#" style="color:gray" onclick="document.cookie='showdesktop=1';document.location.href=document.location.href;" class="ui-link">
+	     Return to Desktop Site</a>
+	 </div>
+	</div><!-- /content -->
+</div><!-- /page -->
 
 <?php include 'mobilecategorypage.php'; ?>
-
 <?php include 'mobilesearchpage.php'; ?>
-
 <?php include 'mobilecartpage.php'; ?>
 
 </body>
