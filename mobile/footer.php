1373d373ce4186cc8122e53376e383bcb742e0ee
jasonfonseca-ontech <jason@ezimerchant.com>
2012-10-30 17:16:05 +1100

    Update mobile/footer.php

--- mobile/footer.php
+++ mobile/footer.php
@@ -1,6 +1,6 @@
 	<div id="page_footer" style="text-align:center;padding-top:10px;">
 	     <a href="#" style="color:gray" onclick="document.cookie='showdesktop=1';document.location.href=document.location.href;" class="ui-link">
-	     <?php echo $_['Return to Desktop Site'] ?></a>
+	     <?php echo $_['Return to Desktop site'] ?></a>
 	 </div>
 	</div><!-- /content -->
 </div><!-- /page -->
